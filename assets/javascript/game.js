950672d17e7f12dc7cf76119a3bf6f4cd98a0fc5
Bobby H <rhoffman103@gmail.com>
2018-06-28 16:37:49 -0400

    detect and display correct letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var player = {
     wins: 0,
     losses: 0,
-    guessesLeft: 0,
+    guessesLeft: 8,
     guesses: [],
     letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     duplicateGuess: false,
@@ -23,8 +23,12 @@ var player = {
     isLetter: function (guess) {
         if (this.letters.indexOf(guess.toLowerCase() >= 0)) {
             validGuess = true;
+            return true;
         } else {
             validGuess = false;
+            return false;
+    //FIXME alert does not pop up
+            alert("Choose a letter!")
         }
     }
 }
@@ -40,6 +44,7 @@ var computer = {
         console.log(chosenWord);
     },
     disguiseWord: function (word) {
+        this.hiddenLetters = "";
         for (i = 0; i < word.length; i++) {
             if (this.space.includes(word[i])) {
                 this.hiddenLetters += word[i];
@@ -52,19 +57,27 @@ var computer = {
 }
 
 const resetGame = function () {
-    player.guessesLeft = 10;
+    player.guessesLeft = 8;
     computer.chooseWord();
     player.clearGuesses();
 };
 
-// FIXME method to unhide hidden letters
-const checkCorrectGuess = function (guess) {
-    if (guess.includes(chosenWord)) {
-                hiddenLetters = hiddenLetters.replace(hiddenLetters.indexOf(i), player.userGuess);
-            }
-            
+// method to unhide correctly guessed letters
+const checkCorrectGuess = function () {
+    var space = " ";
+    computer.hiddenLetters = "";
 
-}
+    for (i=0; i < chosenWord.length; i++) {
+        if (player.guesses.indexOf(chosenWord[i]) >= 0 ) {
+            console.log("guessed a letter");
+            computer.hiddenLetters += chosenWord[i];
+        } else if (space.includes(chosenWord[i])) {
+                computer.hiddenLetters += chosenWord[i];
+            } else {
+            computer.hiddenLetters += "-";
+        }
+    }
+};
 
 // method to check for incorrect letters and update guessesLeft
 // if else method to check for game won or lost
@@ -74,7 +87,7 @@ const updateDOM = function () {
     document.querySelector("#losses").innerHTML = player.losses;
     document.querySelector("#guesses-remaining").innerHTML = player.guessesLeft;
     document.querySelector("#guesses").innerHTML = player.guesses.join(", ");
-    document.querySelector("#hidden-word").innerHTML = hiddenLetters;
+    document.querySelector("#hidden-word").innerHTML = computer.hiddenLetters;
 }
 
 // Start Game
@@ -87,6 +100,11 @@ document.onkeyup = function (event) {
     player.isLetter(userGuess);
     player.checkDuplicateGuess(userGuess);
     player.addGuess(userGuess);
-    computer.disguiseWord(this.chosenWord);
+    checkCorrectGuess();
     updateDOM();
+    if (validGuess) {
+        console.log("Is letter is true");
+    } else if (!validGuess) {
+        console.log("isLetter is False");
+    }
 }
